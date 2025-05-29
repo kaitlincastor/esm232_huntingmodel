de767880a065beb39eba1169699bd796936bc2e9
kaitlincastor <kncastor@gmail.com>
2025-05-29 08:36:12 -0700

    upload function

---
+++ R/lotvmodH.R
@@ -0,0 +1,25 @@
+#' Lot. Voltera Model with hunting 
+#'
+#' function computes the rate of change of populations in a predictor prey interaction
+#' @param t  time (days)
+#' @param pop datatype list initial conditions; list with two values prey=number of prey and pred=number of predictor
+#' @param pars datatype list  coefficient in Lotka-Voltera pars$rprey, pars$alpha, pars$eff, par$pmort
+#'  \emph{rprey} is growth rate of prey population;
+#'  \emph{eff} is the rate of ingestion of prey by predators
+#'  \emph{alpha} is a interaction coefficient (higher values greater interaction
+#'  \emph{hunt} is the rate the prey is being hunted
+# ’  \emph{pmort}  mortality rate of predictor population
+#'
+#' @return  lotvmod returns a list containing the following components
+#' \describe{
+#' \item{dprey}{rate of change of prey populutation}
+#' \item{dpred}{rate of change of preditor populutation}
+#' }
+
+lotvmodK <- function(t, pop, pars) {
+  with(as.list(c(pars, pop)), {
+    dprey <- rprey * (1 - prey / K) * prey - alpha * prey * pred - hunt * prey
+    dpred <- eff * alpha * prey * pred - pmort * pred
+    return(list(c(dprey, dpred)))
+  })
+}
